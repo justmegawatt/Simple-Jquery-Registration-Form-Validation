3462d115e68af32de88851ad05c2283d2b8735df
Megg Gawat <megawatt@live.com>
2018-03-20 12:52:37 -0400

    Separated scripts and styles, completed validation for everything

---
+++ scripts.js
@@ -0,0 +1,38 @@
+function isValidEmailAddress(emailAddress) {
+    var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+    return pattern.test(emailAddress);
+}
+
+$('#validationForm').submit(function(event) {
+    event.preventDefault();
+    let errorFound = false;
+
+    if (!isValidEmailAddress($('#email').val())) {
+        $('#emailError').html('Incorrect Email');
+        errorFound = true;
+    } else {
+        $('#emailError').html('');
+    }
+
+    if (!$.isNumeric($('#telephone').val())) {
+        $('#telephoneError').html('Incorrect Phone (numbers only)');
+        errorFound = true;
+    } else {
+        $('#telephoneError').html('');
+    }
+
+    if ($('#password').val() != $('#confirm').val() || $('#confirm').val() == '') {
+        $('#confirmError').html('Incorrect Password');
+        errorFound = true;
+    } else {
+        $('#confirmError').html('');
+    }
+
+    if (errorFound) {
+        alert('Issues found, please correct the following issues before proceeding');
+    } else {
+        location.reload();
+        alert('Success!');
+    }
+
+});
